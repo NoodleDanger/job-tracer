244033f51812cac397347b81a61db849376f1480
meekle <meekle@mac.com>
2021-07-15 00:53:00 -0500

    query written to sorty by userId, hard coded to 1 until we have users

--- server/controllers/jobApplicationController.js
+++ server/controllers/jobApplicationController.js
@@ -7,13 +7,16 @@ const jobApplicationController = {};
 
 jobApplicationController.getJobApplications = (req, res, next) => {
   // create query string
+  // get user_id from cookies
+  const userId = 1; // hard coded, please change when users
   const queryStr = `
     SELECT * FROM applications
+    WHERE user_id=$1
     `;
 
   // call db query passing in query string
 
-  db.query(queryStr)
+  db.query(queryStr, [userId])
     .then((data) => {
       // add the data to res.locals
       console.log(data.rows);
@@ -77,8 +80,18 @@ jobApplicationController.createJobApplication = (req, res, next) => {
 
   // make query string
 
-  const queryStr =
-    'INSERT INTO applications (company_name,job_title,salary,description,post_source,status_name, status_date, notes, favorite,user_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)';
+  const queryStr = `INSERT INTO applications 
+  (company_name,
+    job_title,
+    salary,
+    description,
+    post_source,
+    status_name, 
+    status_date, 
+    notes, 
+    favorite,
+    user_id) 
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`;
 
   // call db query passing in query string and values array
 
